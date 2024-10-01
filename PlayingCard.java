2db8cd1bc671720742601038524d768db611ff67
Matthew Rueben <mrueben@nmsu.edu>
2024-09-30 23:39:37 -0700

    Rewrite PlayingCard using enums, breaking all other classes.

--- PlayingCard.java
+++ PlayingCard.java
@@ -7,29 +7,61 @@
  */
 public class PlayingCard
 {
-    // Things that define standard playing cards.
-    public static String[] SUITS = {"S", "C", "H", "D"}; // spades, clubs, hearts, diamonds.
-    public static int[] RANKS = {1,2,3,4,5,6,7,8,9,10,11,12,13};  // ace = 1, jack = 11, queen = 12, king = 13.
-    public static int NUM_CARDS_IN_DECK = SUITS.length * RANKS.length;  // 52.  
-    
-    private int rank;
-    private String suit;
+    public enum Rank
+    {
+        ONE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING,
+        ACE;
 
-    public PlayingCard(int rank, String suit)
+        boolean follows(Rank otherRank)
+        {
+            if (this.ordinal() > otherRank.ordinal()
+                    && this.ordinal() - otherRank.ordinal() == 1)
+                    return true;
+            else if (this == ONE && otherRank == ACE) return true;
+            else return false;
+        }
+    }
+
+    public enum Suit
+    {
+        SPADES,
+        CLUBS,
+        HEARTS,
+        DIAMONDS;
+    }
+
+    // Things that define standard playing cards.
+    //public static String[] SUITS = {"S", "C", "H", "D"}; // spades, clubs, hearts, diamonds.
+    //public static int[] RANKS = {1,2,3,4,5,6,7,8,9,10,11,12,13};  // ace = 1, jack = 11, queen = 12, king = 13.
+    //public static int NUM_CARDS_IN_DECK = SUITS.length * RANKS.length;  // 52.
+    
+    private Rank rank;
+    private Suit suit;
+
+    public PlayingCard(Rank rank, Suit suit)
     {
         this.rank = rank;
         this.suit = suit;
     }
 
-    public String toString()
-    {
-        return rank + suit;
-    }
+    public String toString() { return rank + " of " + suit; }
 
     // Tests constructor and printing.
     public static void main(String[] args)
     {
-        PlayingCard myCard = new PlayingCard(12, "S");
+        PlayingCard myCard = new PlayingCard(Rank.ACE, Suit.SPADES);
         System.out.println(myCard);
     }
 
