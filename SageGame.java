4fb285a9baf6a29a2ccc94ddb0308a76e6e82214
Matthew Rueben <mrueben@nmsu.edu>
2024-12-02 00:01:28 -0800

    Game now deals a deck onto the board. Abandoned trying to make board a singleton, or to make its fields final.

--- SageGame.java
+++ SageGame.java
@@ -17,75 +17,75 @@ public class SageGame
 
     public class SageBoard
     {
-        private final List<List<Stack<PlayingCard>>> pileGrid;
-        private final PlayingCard trumpCard;
+        private List<List<Stack<PlayingCard>>> pileGrid;
+        private PlayingCard trumpCard;
         private int numOfTrashes = 2;
 
-        private SageBoard(Deck deck)
+        public void dealCards(Deck deck)
         {
-            pileGrid = new ArrayList<List<Stack<PlayingCard>>>();
+            this.pileGrid = new ArrayList<List<Stack<PlayingCard>>>();
 
-            pileGrid.add(new ArrayList<Stack<PlayingCard>>(3));  // TOP_ROW
+            this.pileGrid.add(new ArrayList<Stack<PlayingCard>>(3));  // TOP_ROW
             // // // // // // //
-            pileGrid.get(TOP_ROW).add(new Stack<PlayingCard>());  // LEFT_COLUMN
-            deck.selectPile(pileGrid.get(TOP_ROW).get(LEFT_COLUMN));
+            this.pileGrid.get(TOP_ROW).add(new Stack<PlayingCard>());  // LEFT_COLUMN
+            deck.selectPile(this.pileGrid.get(TOP_ROW).get(LEFT_COLUMN));
             deck.setNumberOfCardsToDeal(8);
             deck.dealPile();
             // // // // // // //
-            pileGrid.get(TOP_ROW).add(new Stack<PlayingCard>());  // MIDDLE_COLUMN
-            deck.selectPile(pileGrid.get(TOP_ROW).get(MIDDLE_COLUMN));
+            this.pileGrid.get(TOP_ROW).add(new Stack<PlayingCard>());  // MIDDLE_COLUMN
+            deck.selectPile(this.pileGrid.get(TOP_ROW).get(MIDDLE_COLUMN));
             deck.setNumberOfCardsToDeal(8);
             deck.dealPile();
             // // // // // // //
-            pileGrid.get(TOP_ROW).add(new Stack<PlayingCard>());  // RIGHT_COLUMN
-            deck.selectPile(pileGrid.get(TOP_ROW).get(RIGHT_COLUMN));
+            this.pileGrid.get(TOP_ROW).add(new Stack<PlayingCard>());  // RIGHT_COLUMN
+            deck.selectPile(this.pileGrid.get(TOP_ROW).get(RIGHT_COLUMN));
             deck.setNumberOfCardsToDeal(8);
             deck.dealPile();
 
-            pileGrid.add(new ArrayList<Stack<PlayingCard>>(3));  // MIDDLE_ROW
+            this.pileGrid.add(new ArrayList<Stack<PlayingCard>>(3));  // MIDDLE_ROW
             // // // // // // //
-            pileGrid.get(MIDDLE_ROW).add(new Stack<PlayingCard>());  // LEFT_COLUMN
-            deck.selectPile(pileGrid.get(MIDDLE_ROW).get(LEFT_COLUMN));
+            this.pileGrid.get(MIDDLE_ROW).add(new Stack<PlayingCard>());  // LEFT_COLUMN
+            deck.selectPile(this.pileGrid.get(MIDDLE_ROW).get(LEFT_COLUMN));
             deck.setNumberOfCardsToDeal(7);
             deck.dealPile();
             // // // // // // //
-            pileGrid.get(MIDDLE_ROW).add(new Stack<PlayingCard>());  // MIDDLE_COLUMN
-            deck.selectPile(pileGrid.get(MIDDLE_ROW).get(MIDDLE_COLUMN));
+            this.pileGrid.get(MIDDLE_ROW).add(new Stack<PlayingCard>());  // MIDDLE_COLUMN
+            deck.selectPile(this.pileGrid.get(MIDDLE_ROW).get(MIDDLE_COLUMN));
             deck.setNumberOfCardsToDeal(6);
             deck.dealPile();
             // // // // // // //
-            pileGrid.get(MIDDLE_ROW).add(new Stack<PlayingCard>());  // RIGHT_COLUMN
-            deck.selectPile(pileGrid.get(MIDDLE_ROW).get(RIGHT_COLUMN));
+            this.pileGrid.get(MIDDLE_ROW).add(new Stack<PlayingCard>());  // RIGHT_COLUMN
+            deck.selectPile(this.pileGrid.get(MIDDLE_ROW).get(RIGHT_COLUMN));
             deck.setNumberOfCardsToDeal(5);
             deck.dealPile();
 
-            pileGrid.add(new ArrayList<Stack<PlayingCard>>(3));  // BOTTOM_ROW
+            this.pileGrid.add(new ArrayList<Stack<PlayingCard>>(3));  // BOTTOM_ROW
             // // // // // // //
-            pileGrid.get(BOTTOM_ROW).add(new Stack<PlayingCard>());  // LEFT_COLUMN
-            deck.selectPile(pileGrid.get(BOTTOM_ROW).get(LEFT_COLUMN));
+            this.pileGrid.get(BOTTOM_ROW).add(new Stack<PlayingCard>());  // LEFT_COLUMN
+            deck.selectPile(this.pileGrid.get(BOTTOM_ROW).get(LEFT_COLUMN));
             deck.setNumberOfCardsToDeal(4);
             deck.dealPile();
             // // // // // // //
-            pileGrid.get(BOTTOM_ROW).add(new Stack<PlayingCard>());  // MIDDLE_COLUMN
-            deck.selectPile(pileGrid.get(BOTTOM_ROW).get(MIDDLE_COLUMN));
+            this.pileGrid.get(BOTTOM_ROW).add(new Stack<PlayingCard>());  // MIDDLE_COLUMN
+            deck.selectPile(this.pileGrid.get(BOTTOM_ROW).get(MIDDLE_COLUMN));
             deck.setNumberOfCardsToDeal(3);
             deck.dealPile();
             // // // // // // //
-            pileGrid.get(BOTTOM_ROW).add(new Stack<PlayingCard>());  // RIGHT_COLUMN
-            deck.selectPile(pileGrid.get(BOTTOM_ROW).get(RIGHT_COLUMN));
+            this.pileGrid.get(BOTTOM_ROW).add(new Stack<PlayingCard>());  // RIGHT_COLUMN
+            deck.selectPile(this.pileGrid.get(BOTTOM_ROW).get(RIGHT_COLUMN));
             deck.setNumberOfCardsToDeal(2);
             deck.dealPile();
 
-            trumpCard = deck.dealCard();
+            this.trumpCard = deck.dealCard();
         }
     }
 
-    private final SageBoard board;
+    private final SageBoard board = new SageBoard();
 
     public SageGame()
     {
         Deck deck = Deck.getNewUnshuffledDeck().shuffle();
-        this.board = new SageBoard(deck);
+        this.board.dealCards(deck);
     }
     
     //private void dealPile(int numCardsInPile) {
